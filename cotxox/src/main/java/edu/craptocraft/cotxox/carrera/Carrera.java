80818b31b5a2f32202ff2f9a3d9ba78bad464fea
Adrián <adrianlomu2004@gmail.com>
2023-02-13 20:41:06 +0100

    featureConfig(Carrera): implementar primera historia de usuario.

---
+++ cotxox/src/main/java/edu/craptocraft/cotxox/carrera/Carrera.java
@@ -0,0 +1,53 @@
+package edu.craptocraft.cotxox.carrera;
+
+public class Carrera {
+
+    public String tarjetaCredito;
+    public String origen;
+    public String destino;
+    public double distancia;
+    public int tiempoEsperadoMinutos;
+
+
+
+    public Carrera(String tarjetaCredito) {
+        this.tarjetaCredito = tarjetaCredito;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public void setDistancia(double distancia) {
+        this.distancia = distancia;
+    }
+
+    public void setTiempoEsperado(int tiempoEsperadoMinutos) {
+        this.tiempoEsperadoMinutos = tiempoEsperadoMinutos;
+    }
+
+    public String getTarjetaCredito() {
+        return this.tarjetaCredito;
+    }
+
+    public String getOrigen() {
+        return this.origen;
+    }
+
+    public String getDestino() {
+        return this.destino;
+    }
+
+    public double getDistancia() {
+        return this.distancia;
+    }
+
+    public int getCosteEsperado() {
+        return this.tiempoEsperadoMinutos;
+    }
+
+}
